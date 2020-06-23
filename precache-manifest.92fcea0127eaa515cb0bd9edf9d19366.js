4328c363934fd95d09cdfdd3370b8731ed399ae6
Denis <neorimne@gmail.com>
2020-06-23 13:56:43 +0200

    Updates

---
+++ precache-manifest.92fcea0127eaa515cb0bd9edf9d19366.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8526f8b3158ed50dc8fde8996ba080",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "4d4f9c23ffe6f7a753ff",
+    "url": "/Weather-app/static/css/main.3b520791.chunk.css"
+  },
+  {
+    "revision": "20044eeefd557e957c48",
+    "url": "/Weather-app/static/js/2.a256f187.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Weather-app/static/js/2.a256f187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d4f9c23ffe6f7a753ff",
+    "url": "/Weather-app/static/js/main.a4c634e7.chunk.js"
+  },
+  {
+    "revision": "9d2b0e576db66cd702e0",
+    "url": "/Weather-app/static/js/runtime-main.6d0542b5.js"
+  },
+  {
+    "revision": "4736405982e9ff2bd277190149dac98a",
+    "url": "/Weather-app/static/media/mist.47364059.png"
+  },
+  {
+    "revision": "aa9d8bfe0e972731093f07b53a1a0f41",
+    "url": "/Weather-app/static/media/rain.aa9d8bfe.png"
+  },
+  {
+    "revision": "273418751ac21ad3ed170a68a98b1ee6",
+    "url": "/Weather-app/static/media/sun.27341875.png"
+  }
+]);
